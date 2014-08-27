5832949bd707318b3f226c4efc12924bc0e44725
Kurt DaCosta <kurtbradd@gmail.com>
2014-08-26 22:18:56 -0400

    dummy client side socket code

--- public/js/client.js
+++ public/js/client.js
@@ -1,11 +1,14 @@
-var socket = io.connect('http://localhost');
-
-socket.on('connect', function() {
-	console.log('connected');
-	// $('#status').text('Ready')
-})
-
-
 $(function () {
-    // do stuff after DOM has loaded
+
+	var socket = io.connect('http://localhost');
+
+	socket.on('connect', function() {
+		console.log('connected');
+		$('#status').text('Ready')
+	})
+
+	socket.on('newPic', function (data) {
+		
+	})
+
 });
