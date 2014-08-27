2e25162761985d85c5c5af7ac48b5bbb80f4b47d
Kurt DaCosta <kurtbradd@gmail.com>
2014-08-26 21:09:14 -0400

    moved app config to server.js

---
+++ config/server.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+exports.setup = function (app, express) {
+	// all environments
+	app.set('port', process.env.PORT || 3000);
+	// app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.json());
+	app.use(express.urlencoded());
+	app.use(express.methodOverride());
+	app.use(express.static('./public'));
+	app.use(app.router);
+
+	// development only
+	if ('development' == app.get('env')) {
+	  app.use(express.errorHandler());
+	}
+}
