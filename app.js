20b2b074226a0139555817dd0c6eca2a55242784
Kurt DaCosta <kurtbradd@gmail.com>
2014-08-26 22:49:08 -0400

    instagram callback routes

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ var http = require('http');
 var app = express();
 serverConfig.setup(app, express);
 
+app.post('/pics', function (req, res) {
+	res.send(200);
+})
+
+app.get('/pics', function (req, res) {
+	console.log(req.query);
+	res.send(200);
+})
+
 app.get('*', function (req, res) {
 	res.sendfile('./public/views/index.html');
 });
@@ -21,3 +30,4 @@ server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
+var insta = require('./config/instagram.js').setup();
