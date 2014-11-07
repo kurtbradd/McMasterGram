ab7040d7f8c7bd64ec55daadc464e0c5adb9d69c
Kurt DaCosta <kurtbradd@gmail.com>
2014-11-06 22:01:18 -0500

    implemented arrayDifference method

--- config/helpers.js
+++ config/helpers.js
@@ -1,7 +1,16 @@
+var _ = require('lodash');
+
 exports.isString = function (string) {
 	return (typeof string == 'string' || string instanceof String);
 }
 
 exports.objToJSON = function (object) {
 	return JSON.stringify(object, null, 4);
+}
+
+exports.arrayDifferenceById = function (array1, array2) {
+	var diff = _.difference(_.pluck(array1, "id"), _.pluck(array2, "id"));
+	return _.filter(array1, function(obj) {
+		return diff.indexOf(obj.id) >= 0;
+	});
 }
