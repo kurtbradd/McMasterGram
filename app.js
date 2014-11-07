f6d817eb0ff760869b798ba903c822a7180c18a1
Kurt DaCosta <kurtbradd@gmail.com>
2014-11-06 21:59:48 -0500

    removed old express routing

--- app.js
+++ app.js
@@ -3,31 +3,25 @@
  * Module dependencies.
  */
 
-var serverConfig = require('./config/server.js');
+var expressConfig = require('./config/expressConfig.js');
 var express = require('express');
 var http = require('http');
 
+// Create & Configure Express Environment
 var app = express();
-serverConfig.setup(app, express);
+expressConfig.setup(app, express);
 
-app.post('/pics', function (req, res) {
-	res.send(200);
-})
-
-app.get('/pics', function (req, res) {
-	console.log(req.query);
-	res.send(200);
-})
-
-app.get('*', function (req, res) {
-	res.sendfile('./public/views/index.html');
-});
-
+// Create Server
 var server = http.createServer(app)
-var sockets = require('./config/sockets.js').init(server)
-
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
+// Bind Socket.io to HTTP Server
+var sockets = require('./config/sockets.js').init(server)
+
+// Setup Instagram Client
 var insta = require('./config/instagram.js').setup();
+
+// Init Routes with Express App & Socket.io
+require('./controllers/routes.js')(app, sockets);
