66769b67fbc56d58545ed9f54595d96ff6f44800
Kurt DaCosta <kurtbradd@gmail.com>
2014-11-06 22:00:13 -0500

    socket.io passed to instagram controller

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,10 +1,9 @@
-var InstagramController = require('./InstagramController.js');
-
 module.exports = function (app, socketio) {
 
-	console.log('Routes Loaded');
-	
+	console.log('Routes Loaded');	
 	// InstagramController Routes
+	var InstagramController = require('./InstagramController.js');
+	InstagramController.initSockets(socketio);
 	app.get('/pics', InstagramController.getPics);
 	app.post('/pics', InstagramController.postPics(socketio));
 
