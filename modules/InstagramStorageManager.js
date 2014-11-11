5a6868729f4cab00c56f28f81b98e5b1fe448e36
Kurt DaCosta <kurtbradd@gmail.com>
2014-11-11 17:52:01 -0500

    remove unused obj keys pre-storage

--- modules/InstagramStorageManager.js
+++ modules/InstagramStorageManager.js
@@ -12,20 +12,26 @@ exports.initWithSocket = function (socket) {
 	socketServer = socket;
 }
 
-exports.parseDataForImages = function (data) {
-	console.log(data.data.length);
-	setNewImages(data.data);
+exports.parseDataForImages = function (data, callback) {
+	setNewImages(data.data, callback);
 }
 
-exports.setNewImages = setNewImages = function (imageData) {
+setNewImages = function (imageData, callback) {
 	var diff = helper.arrayDifferenceById(imageData, recentImages);
-	_.forEach(diff, function (image) {
+	var compressData = _.map(diff, function(image) { return pluckRequiredData(image); });
+	
+	if (callback&& compressData.length > 0) callback(compressData);
+	_.forEach(compressData, function (image) {
 		recentImages.push(image);
-		// add image to redis
 	});
+	
 	if (recentImages.length > 1000) {
 		recentImages.splice(0,recentImages.length-1000);
 	}	
+}
+
+pluckRequiredData = function (imageData) {
+	return _.pick(imageData, ['id']);
 }
 
 exports.pushMediaUpdatesToSockets = function (sockets) {
