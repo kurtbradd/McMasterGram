823c6d922eccbd0871706c2c0e3cb9a14a89e0fa
Kurt DaCosta <kurtbradd@gmail.com>
2014-11-11 17:52:33 -0500

    additional message listeners, debug WS transport error

--- public/js/client.js
+++ public/js/client.js
@@ -1,14 +1,23 @@
 $(function () {
-	var socket = io.connect('http://9f66a41.ngrok.com');
-	// var socket = io.connect('http://localhost');
+	// var socket = io.connect('http://a37d312.ngrok.com/');
+	var socket = io.connect('http://localhost:3000');
 
 	socket.on('connect', function() {
 		console.log('connected');
 		$('#status').text('Ready')
 	})
 
-	socket.on('newPic', function (data) {
-		
+	socket.on('disconnect', function() {
+		console.log('disconnected');
+	})
+
+	socket.on('error', function (err) {
+	  if (err) console.log(err);
+	});
+
+	socket.on('newPics', function (data) {
+		$('#status').text('Got Data');
+		console.log(data);
 	})
 
 });
