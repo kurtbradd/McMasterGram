01224b08c0950e1e145d6fb7851ac3539b0eb815
Kurt DaCosta <kurtbradd@gmail.com>
2014-11-16 12:46:21 -0500

    added handler to fetch previous pictures
    - cleaned up old code
    - need to migrate to react.js

--- public/js/client.js
+++ public/js/client.js
@@ -1,7 +1,7 @@
 $(function () {
-	var socket = io.connect('http://6c9ab774.ngrok.com');
+	var socket = io.connect('http://51b4a6a.ngrok.com');
 	// var socket = io.connect('http://localhost:3000');
-	var count = 0;
+
 	var imageSet = new SortedSet({ comparator: function(a, b) { 
 		return b - a; 
 	}});
@@ -23,45 +23,46 @@ $(function () {
 		if (err.description) console.log(err.description);
 	});
 
-	socket.on('newPics', function (data) {
+	var recievePictureHandler = function (data) {
 		$('#status').text('Got Data');
-		console.log('new pics' + data.length);
-		count+= data.length;
-		console.log('total count = ' + count);
-		data.forEach(function (item) {
+		data.reverse().forEach(function (item) {
 			storeImageToArray(item);
 			imageSet.insert(item);
 			var dom = createImageElement(item);
 			if (dom) $('#image-gallery').prepend(dom);
 		})
-	})
+	}
 
-	storeImageToArray = function (image) {
+	socket.on('oldPics', recievePictureHandler);
+	socket.on('newPics', recievePictureHandler);
+
+	var storeImageToArray = function (image) {
 		imagesArray.unshift(image);
 		if (imagesArray.length > 100) imagesArray.slice(0, 99);
 	}
 
-
-	createImageElement = function (image) {
+	var createImageElement = function (image) {
+		if (!image) return false;
 		var imageObj = JSON.parse(image);
-		console.log(imageObj);
 		if (!imageObj.type == 'image') return false;
-    var div = document.createElement('div');
-    var a = document.createElement('a');
-    var img = document.createElement('img');
+		console.log(imageObj.created_time);
+		var doc = document; //performance measure
+    var div = doc.createElement('div');
+    var a = doc.createElement('a');
+    var img = doc.createElement('img');
+
     div.setAttribute('class', 'col-lg-3 col-md-4 col-xs-3');
     a.setAttribute('class', 'thumbnail');   
     a.setAttribute('href', imageObj.link);
     a.setAttribute('target', '_blank');
     img.setAttribute('class', 'img-responsive');
-    // standard_resolution
     img.setAttribute('src', imageObj.images.standard_resolution.url);
-    // img.setAttribute('src', 'http://kurtbradd.com/assets/img/break-things.jpg');
+    
     a.appendChild(img);
     div.appendChild(a);
 		return div;
 	}
 
-	$( "#target" ).click(itterateThroughSet);
+	// $( "#target" ).click(itterateThroughSet);
 
 });
